27006863f9ec661ef556e65ffd364501dd81b5d2
LCY Vincent <lauchunyin@gmail.com>
2015-01-07 13:11:51 +0800

    manual build

--- README.md
+++
@@ -1,8 +0,0 @@
-This is for citizen to easily calculate how are they affected by budget policy 2014-15
-
-Input Welcome
-- Report a bug if you find calculations incorrect
-- English localization
-- Let us know how you feel
-
-Powered by - Code for Hong Kong
--- pre-dist/scripts/concat.js
+++
@@ -1,8 +0,0 @@
-require(['angular', 'app'], function(angular) {
-    console.log('bootstrap site');
-    var app = angular.module('budget2014')
-
-    angular.bootstrap(document, ["budget2014"]);
-
-    if(typeof(console)==='undefined'){console=function() {};};
-});
